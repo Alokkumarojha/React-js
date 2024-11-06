c8dc92efa9b92bf825511141c12d9246b1115383
aojha@Shyam <aojha@gmail.com>
2024-11-06 14:44:43 +0530

    Project pending

---
+++ 05currencyConvertor/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-400">Currency convertor</h1>
+    </>
+  );
+}
+
+export default App;
