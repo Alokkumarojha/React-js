b66db6e5f16383ce42bed54e901d75d4229506e3
aojha@Shyam <aojha@gmail.com>
2024-11-06 14:40:40 +0530

    push to github

--- 01Counter/src/App.jsx
+++ 01Counter/src/App.jsx
@@ -1,37 +1,64 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-  let [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
 
-  let increseNumber = () => {
-    if (count < 20) {
-      setCount((prevCounter) => prevCounter + 1);
-      setCount((prevCounter) => prevCounter + 1);
-      setCount((prevCounter) => prevCounter + 1);
-      setCount((prevCounter) => prevCounter + 1);
-    } else {
-      setCount(20);
-    }
-  };
-  let DecreseNumber = () => {
-    if (count > 0) {
-      count = count - 1;
-      setCount(count);
-    } else {
-      setCount(0);
-    }
-  };
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []); // Empty dependency array -> runs only on initial mount
+
   return (
-    <>
-      <h1>Increse and Decrese Number</h1>
-      <h2>Count is: {count}</h2>
-      <button onClick={increseNumber}>Increse</button>
-      <button onClick={DecreseNumber}>Decrese</button>
-    </>
+    <div>
+      <h1>Users:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export default App;
+
+//   let [count, setCount] = useState(0);
+
+//   let increseNumber = () => {
+//     if (count < 20) {
+//       setCount((prevCounter) => prevCounter + 1);
+//       setCount((prevCounter) => prevCounter + 1);
+//       setCount((prevCounter) => prevCounter + 1);
+//       setCount((prevCounter) => prevCounter + 1);
+//     } else {
+//       setCount(20);
+//     }
+//   };
+//   let DecreseNumber = () => {
+//     if (count > 0) {
+//       count = count - 1;
+//       setCount(count);
+//     } else {
+//       setCount(0);
+//     }
+//   };
+
+//   useEffect(() => {
+//     console.log("This logs every render, count is", count);
+//   });
+
+//   return (
+//     <>
+//       <h1>Increse and Decrese Number</h1>
+//       <h2>Count is: {count}</h2>
+//       <button onClick={increseNumber}>Increse</button>
+//       <button onClick={DecreseNumber}>Decrese</button>
+//     </>
+//   );
+// }
+
+// export default App;
